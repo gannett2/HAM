7a25f3683c4d3206bb1a0cbed29ffe7df4f66fc3
Graham Annett <gannett2@uwo.ca>
2018-02-27 19:48:27 -0500

    Added more asset images

--- db/schema.rb
+++ db/schema.rb
@@ -51,18 +51,6 @@ ActiveRecord::Schema.define(version: 20160331180819) do
 
   add_index "pictures", ["user_id"], name: "index_pictures_on_user_id"
 
-  create_table "team_members", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "age"
-    t.string   "church"
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
-  add_index "team_members", ["user_id"], name: "index_team_members_on_user_id"
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
